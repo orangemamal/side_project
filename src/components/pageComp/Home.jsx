7b263c2da6611ab17f858a42f00f0113610de81a
orangemamal <ojm1612@gmail.com>
2024-05-22 10:02:34 +0900

    map fix

--- src/components/pageComp/Home.jsx
+++ src/components/pageComp/Home.jsx
@@ -1,28 +1,55 @@
 import React from 'react';
 import Slick from "../Slick";
+import product1 from 'assets/images/product-1.png';
+import product2 from 'assets/images/product-2.png';
+import product3 from 'assets/images/product-3.png';
 
 const Home = () => {
 
-  const itemsProduct = [ 1, 2, 3];
+  const itemsProduct = [
+    {no: 1, product: '프리미엄 침대형의자', price: 50000},
+    {no: 2, product: '프리미엄 소파형의자', price: 50000},
+    {no: 3, product: '프리미엄 회전의자', price: 50000},
+  ];
+
+  const images = {
+    1: product1,
+    2: product2,
+    3: product3,
+  };
+
+  const postItems = [
+    {no: 1, product: 'Nordic Chair', price: 50000},
+    {no: 2, product: 'Nordic Chair', price: 50000},
+    {no: 3, product: 'Nordic Chair', price: 50000},
+  ]
 
   const servicesItems = [
     {
       keyword: 'truck',
-      title: 'Fast & Free Shipping',
+      title: '빠른 무료 배송',
+      content: '저희는 그 어느 회사보다 빠른 배송을 보장드립니다.',
     },
     {
       keyword: 'bag',
-      title: 'Easy to Shop',
+      title: '쉬운 구매 방법',
+      content: '매장에서 구매하는 것보다 쉽고 빠른 온라인 쇼핑을 제공합니다.',
     },
     {
       keyword: 'support',
-      title: '24/7 Support',
+      title: '365 서비스 지원',
+      content: '24시간 이주',
     },
     {
       keyword: 'return',
-      title: 'Hassle Free Returns',
+      title: '무료 반품 보장',
+      content: '',
     },
   ];
+
+  const formatNumber = (number) => {
+    return new Intl.NumberFormat().format(number);
+  };
 
   return (
     <main>
@@ -36,15 +63,15 @@ const Home = () => {
                 트렌디함을 담은 감성,<br/>
                 가구가 작품이 되는 공간
               </p>
-              <p><a href="shop.html" className="btn">Explore</a></p>
+              <p><a href="shop.html" className="btn btn-primary">살펴보기</a></p>
             </div>
 
             {itemsProduct.map((item, index) => (
               <div className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0" key={index}>
                 <a className="product-item" href="cart.html">
-                  <img src={require(`assets/images/product-${item}.png`)} className="img-fluid product-thumbnail" />
-                  <h3 className="product-title">Nordic Chair</h3>
-                  <strong className="product-price">$50.00</strong>
+                  <img src={require(`assets/images/product-${item.no}.png`)} className="img-fluid product-thumbnail" />
+                  <h3 className="product-title">{ item.product }</h3>
+                  <strong className="product-price">{ formatNumber(item.price) }</strong>
 
                   <span className="icon-cross">
                       <img src={require("assets/icons/cross.svg").default} className="img-fluid" />
@@ -62,10 +89,7 @@ const Home = () => {
           <div className="row justify-content-between">
             <div className="col-lg-6">
               <h2 className="section-title">Why Choose Us</h2>
-              <p>Donec vitae odio quis nisl dapibus malesuada.
-                Nullam ac aliquet velit. Aliquam vulputate velit imperdiet
-                dolor tempor tristique.
-              </p>
+              <p>FurNi가 제공하는 다양한 서비스</p>
 
               <div className="row my-5">
 
@@ -76,7 +100,7 @@ const Home = () => {
                         <img src={require(`assets/icons/${item.keyword}.svg`)} alt="Image" className="imf-fluid" />
                       </div>
                       <h3>{item.title}</h3>
-                      <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
+                      <p>{item.content}</p>
                     </div>
                   </div>
                 ))}
@@ -130,7 +154,11 @@ const Home = () => {
               <div className="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0" key={index}>
                 <div className="product-item-sm d-flex">
                   <div className="thumbnail">
-                    <img src={require(`assets/images/product-${item}.png`)} alt="Image" className="img-fluid" />
+                    <img
+                      src={images[item.no]}
+                      className="img-fluid product-thumbnail"
+                      alt={`Product ${item.no}`}
+                    />
                   </div>
                   <div className="pt-3">
                     <h3>Nordic Chair</h3>
@@ -180,11 +208,11 @@ const Home = () => {
 
           <div className="row">
 
-            {itemsProduct.map((item, index) => (
+            {postItems.map((item, index) => (
               <div className="col-12 col-sm-6 col-md-4 mb-4 mb-md-0" key={index}>
                 <div className="post-entry">
                   <a href="#" className="post-thumbnail">
-                    <img src={require(`assets/images/post-${item}.jpg`)} alt="Image" className="img-fluid" />
+                    <img src={require(`assets/images/post-${item.no}.jpg`)} alt="Image" className="img-fluid" />
                   </a>
                   <div className="post-content-entry">
                     <h3><a href="#">First Time Home Owner Ideas</a></h3>
